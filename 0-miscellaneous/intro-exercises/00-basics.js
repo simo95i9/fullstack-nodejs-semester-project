41f98d5aa14b3febe8f1adc8df3d5c8d652240ed
Simon Gredal <simon.gredal@icloud.com>
2022-08-29 10:24:42 +0200

    Import exercise 1 and 2

---
+++ 0-miscellaneous/intro-exercises/00-basics.js
@@ -0,0 +1,26 @@
+// --------------------------------------
+// Variables, strings, numbers
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+
+
+// Print out the following in the console:
+// My first name is Anders and my last name is Latif
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2021";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2022
+// You cannot touch line 1 or 2
+
+
+
+// --------------------------------------
