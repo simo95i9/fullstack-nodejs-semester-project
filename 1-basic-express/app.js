4e9c7fc9f9146036f526d8bbed65c381240b9016
Simon Gredal <simon.gredal@icloud.com>
2022-08-29 15:23:54 +0200

    Create a basic express.js server

---
+++ 1-basic-express/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/", (request, response) => {
+    const object = {
+        message: "Created my first route in Express.js"
+    }
+    response.send(object)
+})
+
+
+app.listen(3000)
