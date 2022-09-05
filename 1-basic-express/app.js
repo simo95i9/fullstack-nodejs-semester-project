71b61dcee2dbfb19082b55579d7c4ef07eb9f253
Simon Gredal <simon.gredal@icloud.com>
2022-09-05 05:44:31 +0200

    Reformat with prettier

--- 1-basic-express/app.js
+++ 1-basic-express/app.js
@@ -1,13 +1,11 @@
 const express = require("express")
 const app = express()
 
-
 app.get("/", (request, response) => {
-    const object = {
-        message: "Created my first route in Express.js"
-    }
-    response.send(object)
+  const object = {
+    message: "Created my first route in Express.js",
+  }
+  response.send(object)
 })
-
 
 app.listen(3000)
