bdc1be1599669e23e61ef571227b4ba1a9058c92
Simon Gredal <simon.gredal@icloud.com>
2022-10-23 20:37:49 +0200

    First Svelte: Add stuff

---
+++ 5-first-svelte-project/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {Temporal} from "@js-temporal/polyfill";
+    import Date from "./Date.svelte";
+
+    const name = "Simon"
+    const now = Temporal.Now.zonedDateTimeISO()
+</script>
+
+<h1 id="page-title">First Svelte Project</h1>
+<p>My name is {name} and today's date is</p><br>
+<Date></Date>
+
+
+
+<style>
+</style>
---
+++ 5-first-svelte-project/src/Date.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Temporal} from "@js-temporal/polyfill";
+
+    let dates = [Temporal.Now.zonedDateTimeISO()]
+
+    function updateDate() {
+        const now = Temporal.Now.zonedDateTimeISO()
+        dates = [...dates, now]
+    }
+</script>
+
+{#each dates as date}
+    <span>{date.toLocaleString()}</span><br>
+{/each}
+<a href="#" on:click={updateDate}>Update date</a>
+
+<style>
+</style>
